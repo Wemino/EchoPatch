257d39900f310ba88ea71f77e6ba607162ed2b08
Wem <20968054+Wemino@users.noreply.github.com>
2025-03-15 11:35:32 +0100

    CreateFX_Hook: Sprites support

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -445,6 +445,21 @@ static int __stdcall CreateFX_Hook(char* effectType, int fxData, int prop)
 			MemoryHelper::WriteMemory<float>(prop + 0x8, FLT_MAX, false); // m_tmEnd
 			MemoryHelper::WriteMemory<float>(prop + 0xC, FLT_MAX, false); // m_tmLifetime
 		}
+		else if (*reinterpret_cast<uint32_t*>(effectType) == 0x69727053)
+		{
+			char* fxName = MemoryHelper::ReadMemory<char*>(fxData + 0x74, false);
+
+			if (*reinterpret_cast<uint64_t*>(fxName) == 0x75625F656E6F7453 // Stone_bullethole
+		     || *reinterpret_cast<uint64_t*>(fxName) == 0x455F736972626544 // Debris_Electronic_Chunk
+			 || *reinterpret_cast<uint64_t*>(fxName) == 0x575F736972626544 // Debris_Wood_Chunk
+			 || *reinterpret_cast<uint64_t*>(fxName) == 0x4D5F736972626544 // Debris_Mug_Chunk
+			 || *reinterpret_cast<uint64_t*>(fxName) == 0x565F736972626544 // Debris_Vase1_Chunk
+			 || *reinterpret_cast<uint64_t*>(fxName + 0x4) == 0x3674616C70735F68) // Flesh_splat6
+			{
+				MemoryHelper::WriteMemory<float>(prop + 0x8, FLT_MAX, false); // m_tmEnd
+				MemoryHelper::WriteMemory<float>(prop + 0xC, FLT_MAX, false); // m_tmLifetime
+			}
+		}
 	}
 	return CreateFX(effectType, fxData, prop);
 }
